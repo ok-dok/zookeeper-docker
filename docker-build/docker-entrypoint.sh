cbed6df175d66953f1fddfcaeaa8d3b33ca80a49
Shawn Hou <shawn_hou@163.com>
2019-05-09 09:44:28 +0800

    修改变量名前缀，zk服务配置启动时删除原有配置

--- docker-build/docker-entrypoint.sh
+++ docker-build/docker-entrypoint.sh
@@ -3,17 +3,17 @@ set -e
 
 . /etc/profile >/dev/null 2>&1
 
-CONFIG="$ZOO_CONF_DIR/zoo.cfg"
-sed -i "s%dataDir=.*%dataDir=${ZOO_DATA_DIR}%g" $CONFIG
-
-for zoo in $ZOO_SERVERS; do
-    echo "$zoo" >> $CONFIG
+CONFIG="$ZK_CONF_DIR/zoo.cfg"
+sed -i "s%dataDir=.*%dataDir=${ZK_DATA_DIR}%g" $CONFIG
+sed -i '/server.*/d' $CONFIG
+for zk in $ZK_SERVERS; do
+    echo "$zk" >> $CONFIG
 done
 
-# Write myid only if it doesn't exist
-if [ ! -f "$ZOO_DATA_DIR/myid" ]; then
+# Write myid only if $ZOO_MY_ID exist
+if [ -n "${ZK_MY_ID}" ]; then
     mkdir -p $ZOO_DATA_DIR
-    echo "${ZOO_MY_ID:-1}" > "$ZOO_DATA_DIR/myid"
+    echo "${ZK_MY_ID:-1}" > "$ZK_DATA_DIR/myid"
 fi
 
 if [ "$1" = "zkServer.sh" ]; then
